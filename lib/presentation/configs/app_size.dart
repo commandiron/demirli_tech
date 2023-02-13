166f8e4ae399c1921f02bdc33e2bf1bc9057b6dc
commandiron <commandiron@gmail.com>
2023-02-13 14:32:42 +0300

    .

--- lib/presentation/configs/app_size.dart
+++ lib/presentation/configs/app_size.dart
@@ -5,23 +5,10 @@ import '../responsive/responsive.dart';
 class AppSize {
   static double? navBarHeight;
   static double? logoWidth;
-
   static double? homeSectionHeight;
-  static double? homeAnimationStartOffset;
-  static double? homeAnimationEndOffset;
-
   static double? productsSectionHeight;
-  static double? productsAnimationStartOffset;
-  static double? productsAnimationEndOffset;
-
   static double? servicesSectionHeight;
-  static double? servicesAnimationStartOffset;
-  static double? servicesAnimationEndOffset;
-
   static double? aboutSectionHeight;
-  static double? aboutAnimationStartOffset;
-  static double? aboutAnimationEndOffset;
-
   static double? contactSectionHeight;
 
   static void init(BuildContext context) {
@@ -33,17 +20,5 @@ class AppSize {
     servicesSectionHeight = Responsive.isDesktop(context) ? 720 : Responsive.isTablet(context) ? 540 : MediaQuery.of(context).size.height - navBarHeight!;
     aboutSectionHeight = Responsive.isMobile(context) ? MediaQuery.of(context).size.height - navBarHeight! : 720;
     contactSectionHeight =  Responsive.isMobile(context) ? MediaQuery.of(context).size.height - navBarHeight! : 480;
-
-    homeAnimationStartOffset = 0;
-    homeAnimationEndOffset = AppSize.homeSectionHeight! +  AppSize.productsSectionHeight!;
-
-    productsAnimationStartOffset = AppSize.homeSectionHeight! + AppSize.productsSectionHeight! / 2;
-    productsAnimationEndOffset = AppSize.homeSectionHeight! + AppSize.productsSectionHeight! + AppSize.servicesSectionHeight!;
-
-    servicesAnimationStartOffset = AppSize.homeSectionHeight! + AppSize.productsSectionHeight! + AppSize.servicesSectionHeight! / 2;
-    servicesAnimationEndOffset =  AppSize.homeSectionHeight! + AppSize.productsSectionHeight! + AppSize.servicesSectionHeight! + AppSize.aboutSectionHeight!;
-
-    aboutAnimationStartOffset = AppSize.homeSectionHeight! + AppSize.productsSectionHeight! + AppSize.servicesSectionHeight! + AppSize.aboutSectionHeight! / 2;
-    aboutAnimationEndOffset =  AppSize.homeSectionHeight! + AppSize.productsSectionHeight! + AppSize.servicesSectionHeight! + AppSize.aboutSectionHeight! + AppSize.contactSectionHeight! * 2;
   }
 }
