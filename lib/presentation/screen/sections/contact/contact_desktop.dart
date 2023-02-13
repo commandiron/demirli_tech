f647584b9465008e982d761cc843f928e365d2b7
commandiron <commandiron@gmail.com>
2023-02-13 13:37:38 +0300

    .

--- lib/presentation/screen/sections/contact/contact_desktop.dart
+++ lib/presentation/screen/sections/contact/contact_desktop.dart
@@ -22,92 +22,92 @@ class ContactDesktop extends StatelessWidget {
       child:  Column(
         children: [
           SectionTitle(title: BodySection.contact.title),
-          Expanded(
-            child: Padding(
-              padding: AppPadding.horizontalL!,
-              child: SelectionArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Sosyal Medya", style: AppTextStyle.h3!),
-                    AppSpace.verticalM!,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: CompanyContact.items.map(
-                        (companyContact) {
-                          return ContactIconButton(
-                            iconPath: companyContact.iconPath,
-                            iconOriginalColor: companyContact.iconOriginalColor,
-                            onTap: () => openURL(companyContact.url),
-                          );
-                        }
-                      ).toList(),
-                    ),
-                    AppSpace.verticalL!,
-                    Text("Eposta", style: AppTextStyle.h3!),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        AppSpace.horizontalL!,
-                        Text("demirlitech@gmail.com", style: AppTextStyle.b1!),
-                        AppSpace.horizontalM!,
-                        InkWell(
-                          onTap: () => openURL("mailto:demirlitech@gmail.com"),
-                          child: Icon(
-                            Icons.mail,
-                            color: Theme.of(context).colorScheme.primary,
-                            size: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    AppSpace.verticalL!,
-                    Text("Ofis", style: AppTextStyle.h3!),
-                    FittedBox(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          AppSpace.horizontalL!,
-                          Text("Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL", style: AppTextStyle.b1!),
-                          AppSpace.horizontalM!,
-                          InkWell(
-                            onTap: () => openURL("https://goo.gl/maps/RNiqv558Wzu3nqeR7"),
-                            child: Icon(
-                              Icons.location_on,
-                              color: Theme.of(context).colorScheme.primary,
-                              size: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    AppSpace.verticalL!,
-                    Text("Telefon", style: AppTextStyle.h3!),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        AppSpace.horizontalL!,
-                        Text("+90 535 508 55 52", style: AppTextStyle.b1!),
-                        Row(
-                          children: [
-                            AppSpace.horizontalM!,
-                            InkWell(
-                              onTap: () => openURL("tel:+90 535 508 55 52"),
-                              child: Icon(
-                                Icons.phone,
-                                color: Theme.of(context).colorScheme.primary,
-                                size: 16,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ),
-            ),
-          ),
+          // Expanded(
+          //   child: Padding(
+          //     padding: AppPadding.horizontalL!,
+          //     child: SelectionArea(
+          //       child: Column(
+          //         mainAxisAlignment: MainAxisAlignment.center,
+          //         children: [
+          //           Text("Sosyal Medya", style: AppTextStyle.h3!),
+          //           AppSpace.verticalM!,
+          //           Row(
+          //             mainAxisAlignment: MainAxisAlignment.center,
+          //             children: CompanyContact.items.map(
+          //               (companyContact) {
+          //                 return ContactIconButton(
+          //                   iconPath: companyContact.iconPath,
+          //                   iconOriginalColor: companyContact.iconOriginalColor,
+          //                   onTap: () => openURL(companyContact.url),
+          //                 );
+          //               }
+          //             ).toList(),
+          //           ),
+          //           AppSpace.verticalL!,
+          //           Text("Eposta", style: AppTextStyle.h3!),
+          //           Row(
+          //             mainAxisAlignment: MainAxisAlignment.center,
+          //             children: [
+          //               AppSpace.horizontalL!,
+          //               Text("demirlitech@gmail.com", style: AppTextStyle.b1!),
+          //               AppSpace.horizontalM!,
+          //               InkWell(
+          //                 onTap: () => openURL("mailto:demirlitech@gmail.com"),
+          //                 child: Icon(
+          //                   Icons.mail,
+          //                   color: Theme.of(context).colorScheme.primary,
+          //                   size: 16,
+          //                 ),
+          //               ),
+          //             ],
+          //           ),
+          //           AppSpace.verticalL!,
+          //           Text("Ofis", style: AppTextStyle.h3!),
+          //           FittedBox(
+          //             child: Row(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: [
+          //                 AppSpace.horizontalL!,
+          //                 Text("Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL", style: AppTextStyle.b1!),
+          //                 AppSpace.horizontalM!,
+          //                 InkWell(
+          //                   onTap: () => openURL("https://goo.gl/maps/RNiqv558Wzu3nqeR7"),
+          //                   child: Icon(
+          //                     Icons.location_on,
+          //                     color: Theme.of(context).colorScheme.primary,
+          //                     size: 16,
+          //                   ),
+          //                 ),
+          //               ],
+          //             ),
+          //           ),
+          //           AppSpace.verticalL!,
+          //           Text("Telefon", style: AppTextStyle.h3!),
+          //           Row(
+          //             mainAxisAlignment: MainAxisAlignment.center,
+          //             children: [
+          //               AppSpace.horizontalL!,
+          //               Text("+90 535 508 55 52", style: AppTextStyle.b1!),
+          //               Row(
+          //                 children: [
+          //                   AppSpace.horizontalM!,
+          //                   InkWell(
+          //                     onTap: () => openURL("tel:+90 535 508 55 52"),
+          //                     child: Icon(
+          //                       Icons.phone,
+          //                       color: Theme.of(context).colorScheme.primary,
+          //                       size: 16,
+          //                     ),
+          //                   ),
+          //                 ],
+          //               )
+          //             ],
+          //           ),
+          //         ],
+          //       )
+          //     ),
+          //   ),
+          // ),
           const ContactFooter()
         ],
       )
