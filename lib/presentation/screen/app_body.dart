7ea7d93050eda7e8b8d46b22541e39f97b12ab3e
commandiron <commandiron@gmail.com>
2023-02-22 15:15:59 +0300

    .

--- lib/presentation/screen/app_body.dart
+++ lib/presentation/screen/app_body.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:demirli_tech/presentation/cubit/app_cubit.dart';
 import 'package:demirli_tech/presentation/cubit/app_state.dart';
-import 'package:dyn_mouse_scroll/dyn_mouse_scroll.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,30 +14,26 @@ class AppBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AppCubit, AppState>(
       builder: (context, state) {
-        return DynMouseScroll(
-          builder: (p0, p1, p2) {
-            return CustomScrollView(
-              controller: p1,
-              scrollBehavior: const MaterialScrollBehavior().copyWith(
-                dragDevices: {
-                  PointerDeviceKind.mouse,
-                  PointerDeviceKind.trackpad,
-                  PointerDeviceKind.touch
-                },
+        return CustomScrollView(
+          controller: state.scrollController,
+          scrollBehavior: const MaterialScrollBehavior().copyWith(
+            dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.trackpad,
+              PointerDeviceKind.touch
+            },
+          ),
+          slivers: [
+            SliverToBoxAdapter(
+              child: ListView(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: BodySection.values.map(
+                        (section) => section.view
+                ).toList(),
               ),
-              slivers: [
-                SliverToBoxAdapter(
-                  child: ListView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: BodySection.values.map(
-                            (section) => section.view
-                    ).toList(),
-                  ),
-                )
-              ],
-            );
-          },
+            )
+          ],
         );
       },
     );
